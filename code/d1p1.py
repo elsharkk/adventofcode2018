49b5d92cb7a17cca7e7f7f7f392c8254d34f06fb
Eleanor Sluyter-Meade <eleanor.sluyter-meade@inrix.com>
2019-07-23 18:12:56 +0100

    d

--- code/d1p1.py
+++ code/d1p1.py
@@ -1,7 +1,21 @@
 import sys
 
-def solve():
-	print("hey")
+# quick summary of problem
+#	puzzle input is a series of changes in frequency
+#	it is a list of + and -
+#	add them all up 
+# test cases
+#	+1, +1, +1 = 3
+#	+1, +1, -2 = 0
+#	-1, -2, -3 = 6
 
-if __name__ == "__main__":
-	solve()
+def parse_string(line):
+	return int(line)
+
+total = 0
+with open(r'..\data\d1p1.txt', 'r') as data:
+	for line in data.readlines():
+		total = total + parse_string(line)
+
+print('Total of all numbers:',total)
+
